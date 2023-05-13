8c3764011c48ac76e7935c8c70ccb924a2b4ada8
Himanshu vyas <himanshuvyas975@gmail.com>
2023-05-13 20:52:20 +0000

    fetching of secrets keys is migrated to environment variables

--- src/main/java/com/example/demo/controller/Address.java
+++ src/main/java/com/example/demo/controller/Address.java
@@ -1,15 +1,12 @@
 package com.example.demo.controller;
 
-import com.example.demo.data.PersonData;
 import com.example.demo.mapping.UrlMapper;
 import com.example.demo.service.address.AddressService;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
