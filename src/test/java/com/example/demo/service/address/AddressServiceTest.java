bf6539c9d68c0dce0fbe0f2518ef8f5233126837
Himanshu vyas <himanshuvyas975@gmail.com>
2023-06-23 12:42:02 +0000

    test scope for address service in progress

---
+++ src/test/java/com/example/demo/service/address/AddressServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.service.address;
+
+import org.junit.jupiter.api.Test;
+
+public class AddressServiceTest {
+    @Test
+    void testAddressFromId() {
+
+    }
+
+    @Test
+    void testDeleteAddress() {
+
+    }
+
+    @Test
+    void testDeleteAllAddress() {
+
+    }
+
+    @Test
+    void testListAllAddresses() {
+
+    }
+
+    @Test
+    void testSaveAddress() {
+
+    }
+
+    @Test
+    void testUpdateAddress() {
+
+    }
+}
