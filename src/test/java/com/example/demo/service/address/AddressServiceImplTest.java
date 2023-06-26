a436aec0637eb07ef84bd132b03b7e05eb5e283c
RoadRomeo1 <himanshuvyas975@gmail.com>
2023-06-26 21:01:14 +0530

    initiating test scope of services

---
+++ src/test/java/com/example/demo/service/address/AddressServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.service.address;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressServiceImplTest {
+
+    @Test
+    void listAllAddresses() {
+    }
+
+    @Test
+    void addressFromId() {
+    }
+
+    @Test
+    void saveAddress() {
+    }
+
+    @Test
+    void updateAddress() {
+    }
+
+    @Test
+    void deleteAddress() {
+    }
+
+    @Test
+    void deleteAllAddress() {
+    }
+}
---
+++ src/test/java/com/example/demo/service/person/PersonSeviceImplTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.service.person;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonSeviceImplTest {
+
+    @Test
+    void getAllData() {
+    }
+
+    @Test
+    void getData() {
+    }
+
+    @Test
+    void saveData() {
+    }
+
+    @Test
+    void alterData() {
+    }
+
+    @Test
+    void dropAllData() {
+    }
+
+    @Test
+    void dropData() {
+    }
+}
